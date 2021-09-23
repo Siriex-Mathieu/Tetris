3eda73f726af402c4bfb78d1cfc87ab78aa5ec8c
Siriex-Mathieu <60385180+Siriex-Mathieu@users.noreply.github.com>
2021-09-23 14:58:18 +0200

    ajout limite

--- Assets/tetrisBlock.cs
+++ Assets/tetrisBlock.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class tetrisBlock : MonoBehaviour
 {
     private float previousTime;
     public float fallTime = 0.8f;
-    private static int height = 20;
-    private static int width = 10;
+    private static int height = 40;
+    private static int width = 20;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {  
     }
 
     // Update is called once per frame
@@ -20,26 +19,36 @@ public class tetrisBlock : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1,0,0);
+           
+            if(!ValidMove())
+                transform.position -= new Vector3(-1,0,0);
+
         }
         else  if(Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1,0,0);
+           
+            if(!ValidMove())
+                transform.position -= new Vector3(1,0,0);
+
         }
 
         if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
         {
             transform.position += new Vector3(0,-1,0);
+             if(!ValidMove())
+                transform.position -= new Vector3(0,-1,0);
             previousTime = Time.time;
         }
     }
-    bool ValiMove()
+    bool ValidMove()
     {
-        foreach (Transfrom children in transform)
+        foreach (Transform children in transform)
         {
-            int roundedX = Mathf.roundeToInt(children.transform.position.x);
-            int roundedY = Mathf.roundeToInt(children.transform.position.y);
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
 
-            if(roundedX<0 || roundedX>= width || roundedY< 0 || roundedY>+height)
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY>=height)
             {
                 return false;
             }
