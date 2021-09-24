0cf43b2708b84a1befdefb65a0603824524c135e
Siriex-Mathieu <60385180+Siriex-Mathieu@users.noreply.github.com>
2021-09-24 14:56:33 +0200

    ajout de commentaire et des touches

--- Assets/Scripts/SpawnTetrisBlock.cs
+++ Assets/Scripts/SpawnTetrisBlock.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnTetrisBlock : MonoBehaviour
 {
-    public GameObject[] Tetrominos;
+    public GameObject[] Tetrominos;//Objet de type GameObject permetant de faire une liste de GameObject(Liste de block)
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,7 @@ public class SpawnTetrisBlock : MonoBehaviour
     {
     }
 
-    public void NewTetrisBlock(){
+    public void NewTetrisBlock(){//Fait apparaitre un nouveau block a l'endroit du gameObject
         Instantiate(Tetrominos[Random.Range(0, Tetrominos.Length)], transform.position, Quaternion.identity);
     }
 }
--- Assets/Scripts/tetrisBlock.cs
+++ Assets/Scripts/tetrisBlock.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 using System;
 public class tetrisBlock : MonoBehaviour
 {
-    private float previousTime;
-    public float fallTime = 0.8f;
-    private static int height = 40;
-    private static int width = 20;
+    private float previousTime; 
+    public float fallTime = 0.8f;//Temps pour la piece de tomber
+    private static int height = 40; //Hauteur
+    private static int width = 20; //Longueur
 
-    private static Transform[,] grid = new Transform[width,height];
+    private KeyCode gauche = KeyCode.LeftArrow; //Appui sur <- 
+    private KeyCode droite = KeyCode.RightArrow;//Appui sur ->
+    private KeyCode bas = KeyCode.DownArrow;
+    private KeyCode rotationD = KeyCode.D;
+    private KeyCode rotaionG = KeyCode.Q;
 
-    public Vector3 RotationBlock;
+    private static Transform[,] grid = new Transform[width,height]; //Pour les collision entre les block
+
+    public Vector3 RotationBlock; //Rotation
     // Start is called before the first frame update
     void Start()
     {  
@@ -20,46 +26,54 @@ public class tetrisBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if(Input.GetKeyDown(gauche))//Appui sur <- 
         {
-            transform.position += new Vector3(-1,0,0);
+            transform.position += new Vector3(-1,0,0); //Deplace a gauche
            
             if(!ValidMove())
-                transform.position -= new Vector3(-1,0,0);
+                transform.position -= new Vector3(-1,0,0); //Si peut pas deplace a droite (Annule le movement)
 
         }
-        else  if(Input.GetKeyDown(KeyCode.RightArrow))
+        else  if(Input.GetKeyDown(droite)) //Appui sur ->
         {
-            transform.position += new Vector3(1,0,0);
+            transform.position += new Vector3(1,0,0); //Deplace a droite
            
             if(!ValidMove())
-                transform.position -= new Vector3(1,0,0);
+                transform.position -= new Vector3(1,0,0);//Si peut pas deplace a gauche (Annule le movement)
 
         }
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        else if(Input.GetKeyDown(rotationD))//appui sur D
         {
-            transform.RotateAround(transform.TransformPoint(RotationBlock),new Vector3(0,0,1),90);
+            transform.RotateAround(transform.TransformPoint(RotationBlock),new Vector3(0,0,1),90);//Rotation du block a 90° a droite
             if(!ValidMove())
-                 transform.RotateAround(transform.TransformPoint(RotationBlock),new Vector3(0,0,1),-90);
+                 transform.RotateAround(transform.TransformPoint(RotationBlock),new Vector3(0,0,1),-90);//si peut pas 90° a gauche
+
+        }
+         else if(Input.GetKeyDown(rotaionG))//appui sur Q
+        {
+            transform.RotateAround(transform.TransformPoint(RotationBlock),new Vector3(0,0,1),-90);//Rotation du block a 90° a gauche
+            if(!ValidMove())
+                 transform.RotateAround(transform.TransformPoint(RotationBlock),new Vector3(0,0,1),90);//si peut pas 90° a droite
 
         }
 
-        if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        if(Time.time - previousTime > (Input.GetKey(bas) ? fallTime / 10 : fallTime))//Condition si on appuit sur bas ou que le temps de tombe arrive a zero
         {
-            transform.position += new Vector3(0,-1,0);
+            transform.position += new Vector3(0,-1,0);//Deplace vers le bas
+
              if(!ValidMove())
              {
-                transform.position -= new Vector3(0,-1,0);
-               AddToGrid();
-                this.enabled = false;
-                FindObjectOfType<SpawnTetrisBlock>().NewTetrisBlock();
+                transform.position -= new Vector3(0,-1,0);//si peut pas monte 
+               AddToGrid();//regarde si il a un block
+                this.enabled = false;//le bolck ne devient plus le block courant = desactiver
+                FindObjectOfType<SpawnTetrisBlock>().NewTetrisBlock();//Fait spawner un block
              }
-            previousTime = Time.time;
+            previousTime = Time.time;//remet de temps par defaut
         }
     
     }
 
-    void AddToGrid(){
+    void AddToGrid(){//Permet de regarder si il y a un block en dessous
          foreach (Transform children in transform)
         {
             int roundedX = Mathf.RoundToInt(children.transform.position.x);
@@ -67,7 +81,7 @@ public class tetrisBlock : MonoBehaviour
             grid[roundedX,roundedY] = children;
     }
     }
-    bool ValidMove()
+    bool ValidMove()//Regarde si on peut faire un mouvement, ça utilise les cordonners des block 
     {
         foreach (Transform children in transform)
         {
