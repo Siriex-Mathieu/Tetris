9cbd81440b83c8685433089b87b5b03327357d71
Ciberdead <nico_goyon@orange.fr>
2021-10-08 20:58:15 +0200

    Ajout d'un code non fonctionnel
    Cela est une sorte de carcasse du code pouvant être modifier pour le faire fonctionner, il faut par contre tester le code..

--- Assets/Scripts/AccessFile.cs
+++ Assets/Scripts/AccessFile.cs
@@ -1,12 +1,32 @@
+import System.IO;
+using System;
 /**
  * Classe qui permet d'accéder et modifier des fichiers, principalement des fichiers config .cfg
  */
-
 public class AccessFile
 {
     public static void Set(string field, string value, FileName file)
     {
+        System.IO.StreamReader reader = new System.IO.StreamReader(file);
+        string line;
+        System.IO.StreamWriter writer = new System.IO.StreamWriter(file);
+        writer.Close();
+        while ((line = reader.ReadLine()) != string.Empty)
+        {
+            string[] id_value = line.Split('=');
+            switch (id_value[0])
+            {
+                case field
+                    break;
+            }
+        }
+        writer.Write(field + " = "+value);
 
+        reader.Close();
+    }
+    public static void Main(string[] args) {
+        Set("Doss", "Test", FileName.KeySettings);
+        System.Console.WriteLine("test")
     }
 
 
