11d137ed243ca7f56ce6a80ca70dc85d900b7a8f
Ciberdead <nico_goyon@orange.fr>
2021-10-24 17:05:39 +0200

    détection des touches

--- Assets/Scripts/KeySelection.cs
+++ Assets/Scripts/KeySelection.cs
@@ -2,6 +2,7 @@
 // https://docs.unity3d.com/ScriptReference/Event-keyCode.html
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class KeySelection : MonoBehaviour
 {
@@ -9,22 +10,36 @@ public class KeySelection : MonoBehaviour
     [SerializeField] private GameObject getKeyMenu;
 
     [SerializeField] private static bool getKey;
+
+    Settings file = Settings.init();
+
+    private string prin ;
      void Update()
     {        
         if (Input.GetKeyDown(KeyCode.Escape))
             DesactiveMenu();
-
+        if(getKey){
+            if(Input.anyKeyDown){
+                foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+                {
+                    if(Input.GetKeyDown(key)) print(key);
+                    // TODO il faut pouvoir remonter jusqu'à tetris block pour y changer les touches
+                }
+            }
+        }
     }
 
     public void ActiveMenu()
     {
-        getKeyMenu.SetActive(true);            
+        getKeyMenu.SetActive(true);
+        getKey = true;            
         
     }
 
    public void DesactiveMenu()
     {
         getKeyMenu.SetActive(false);
+        getKey = false;
     }
 
 }
