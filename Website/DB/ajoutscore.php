95ef749888f39db72f38e9247c83f5cfca3623fe
robin-semene <robin.semene@etu.umontpellier.fr>
2021-12-29 20:01:37 +0100

    ajout du système pour ajouter les scores à la bd

--- Website/DB/ajoutscore.php
+++ Website/DB/ajoutscore.php
@@ -8,13 +8,11 @@ require_once "../lib/file.php";
 require_once File::build_path(array("model", "model.php"));
 
 try{
-    $sql = "INSERT INTO p_score (pseudo,score) VALUES (':username',':score')";
+    $sql = "INSERT INTO p_score (pseudo,score) VALUES (?,?)";
     $req_prep = Model::getPDO()->prepare($sql);
-    $values = array(
-        "username" => $username,
-        "score" => $score
-    );
-    $req_prep->execute($values);
+    $req_prep->bindParam(1,$username);
+    $req_prep->bindParam(2,$score);
+    $req_prep->execute();
 }
 catch(Throwable $t){
     echo "Erreur :" . $t ;
